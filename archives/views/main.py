7f9f8b31a640cd6d9906c6c05c910d9c338cdb91
nepeat <nepeat@users.noreply.github.com>
2015-06-01 02:21:10 -0800

    Pretty date formatting

--- archives/views/main.py
+++ archives/views/main.py
@@ -8,6 +8,7 @@ from archives.lib.model import Post
 from archives.lib.classes import Page
 
 import json
+from datetime import datetime
 
 blueprint = Blueprint('main', __name__)
 
@@ -15,7 +16,50 @@ blueprint = Blueprint('main', __name__)
 def prettyjson(data):
     return json.dumps(data, indent=4)
 
+def prettydate(time=False):
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    """
+    now = datetime.now()
+    if type(time) is int:
+        diff = now - datetime.fromtimestamp(time)
+    elif isinstance(time, datetime):
+        diff = now - time
+    elif not time:
+        diff = now - now
+    second_diff = diff.seconds
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return ''
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "just now"
+        if second_diff < 60:
+            return str(second_diff) + " seconds ago"
+        if second_diff < 120:
+            return "a minute ago"
+        if second_diff < 3600:
+            return str(second_diff / 60) + " minutes ago"
+        if second_diff < 7200:
+            return "an hour ago"
+        if second_diff < 86400:
+            return str(second_diff / 3600) + " hours ago"
+    if day_diff == 1:
+        return "Yesterday"
+    if day_diff < 7:
+        return str(day_diff) + " days ago"
+    if day_diff < 31:
+        return str(day_diff / 7) + " weeks ago"
+    if day_diff < 365:
+        return str(day_diff / 30) + " months ago"
+    return str(day_diff / 365) + " years ago"
+
 blueprint.add_app_template_filter(prettyjson)
+blueprint.add_app_template_filter(prettydate)
 
 @blueprint.route("/")
 def front():
