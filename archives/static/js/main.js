7d8ccdc7cc6e25e160183f90505f73d9d1b74bc2
nepeat <nepeat@users.noreply.github.com>
2015-06-01 04:17:41 -0800

    Hide debug link unless localStorage is set

--- archives/static/js/main.js
+++ archives/static/js/main.js
@@ -1,1 +1,20 @@
-$(".button-collapse").sideNav({'edge': 'left', closeOnClick: true});
+var haslocalStorage = (function() {
+	var mod = 'modernizr';
+	try {
+		localStorage.setItem(mod, mod);
+		localStorage.removeItem(mod);
+		return true;
+	} catch(e) {
+		return false;
+	}
+})();
+
+$(document).ready(function() {
+	$(".button-collapse").sideNav({'edge': 'left', closeOnClick: true});
+
+	if (haslocalStorage) {
+		if (localStorage.getItem("debug") === "on") {
+			$(".debugbutton").show();
+		}
+	}
+});
