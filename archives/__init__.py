98e2e6d44ea89f8ae90e5ac1aa52c86b5666cd38
nepeat <nepeat@users.noreply.github.com>
2015-12-27 04:06:44 -0900

    Automatic cache breaking and self host Materialize files.

--- archives/__init__.py
+++ archives/__init__.py
@@ -1,12 +1,15 @@
 # This Python file uses the following encoding: utf-8
 from flask import Flask
-from archives.lib.requests import connect_sql, before_request, disconnect_sql
+from archives.lib.requests import connect_sql, before_request, disconnect_sql, cache_breaker
 from archives.views import main
 
 app = Flask(__name__)
 
 # Throw tracebacks to console
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
+# Cache breaking
+app.url_defaults(cache_breaker)
 
 # Request handlers
 app.before_request(connect_sql)
--- archives/lib/requests.py
+++ archives/lib/requests.py
@@ -1,7 +1,9 @@
 # This Python file uses the following encoding: utf-8
+import os
+
 from archives.lib.model import sm, Post
 from sqlalchemy import func
-from flask import g
+from flask import g, request, current_app
 
 def connect_sql():
     g.sql = sm()
@@ -16,3 +18,27 @@ def disconnect_sql(result=None):
         del g.sql
 
     return result
+
+file_hashes = {}
+
+def cache_breaker(endpoint, values):
+    if 'static' == endpoint or endpoint.endswith('.static'):
+        filename = values.get('filename')
+        if filename:
+            if '.' in endpoint:
+                blueprint = endpoint.rsplit('.', 1)[0]
+            else:
+                blueprint = request.blueprint
+
+            if blueprint:
+                static_folder = current_app.blueprints[blueprint].static_folder or current_app.static_folder
+            else:
+                static_folder = current_app.static_folder
+
+            values['q'] = static_file_hash(os.path.join(static_folder, filename))
+
+def static_file_hash(filename):
+    if filename not in file_hashes:
+        file_hashes[filename] = int(os.stat(filename).st_mtime)
+
+    return file_hashes[filename]
