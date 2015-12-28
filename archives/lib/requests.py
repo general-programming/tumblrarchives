bfce764b562848a91ff61b76abafbb7d215a5b92
nepeat <nepeat@users.noreply.github.com>
2015-12-27 16:39:29 -0900

    Regis connection pool.

--- archives/lib/requests.py
+++ archives/lib/requests.py
@@ -1,9 +1,18 @@
 # This Python file uses the following encoding: utf-8
 import os
 
-from archives.lib.model import sm, Post
-from sqlalchemy import func
 from flask import g, request, current_app
+from redis import ConnectionPool
+from sqlalchemy import func
+
+from archives.lib.model import sm, Post
+
+redis_pool = ConnectionPool(
+    host=os.environ.get('REDIS_PORT_6379_TCP_ADDR', os.environ.get('REDIS_HOST', '127.0.0.1')),
+    port=int(os.environ.get('REDIS_PORT_6379_TCP_PORT', os.environ.get('REDIS_PORT', 6379))),
+    db=int(os.environ.get('REDIS_DB', 0)),
+    decode_responses=True
+)
 
 def connect_sql():
     g.sql = sm()
