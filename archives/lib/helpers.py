a11a414cf4b8fe5a0a3d63ce690dd9615e32031a
nepeat <nepeat@users.noreply.github.com>
2015-12-28 22:31:29 -0900

    [helpers] Create helpers file with Tumblr URL parsing.

---
+++ archives/lib/helpers.py
@@ -0,0 +1,17 @@
+import re
+
+validate_url = re.compile(r"(\w+).tumblr.com/post/(\d+)", re.I | re.U)
+
+def parse_tumblr_url(url=None):
+    if not url:
+        return None
+
+    matches = validate_url.search(url)
+
+    if matches:
+        return {
+            "url": matches.group(0),
+            "post_id": matches.group(1)
+        }
+
+    return None
