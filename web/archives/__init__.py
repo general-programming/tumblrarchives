09098cc173294a8111ed7c069f973d9f34cc1d83
nepeat <nepeat@users.noreply.github.com>
2018-12-04 18:56:58 -0800

    Sentry

--- web/archives/__init__.py
+++ web/archives/__init__.py
@@ -2,13 +2,26 @@
 import os
 import traceback
 
+# Initialize Sentry before importing the rest of the app.
+sentry_sdk = None
+
+if "SENTRY_DSN" in os.environ:
+    import sentry_sdk
+    from sentry_sdk.integrations.flask import FlaskIntegration
+    from sentry_sdk.integrations.celery import CeleryIntegration
+
+    sentry_sdk.init(
+        dsn=os.environ["SENTRY_DSN"],
+        integrations=[FlaskIntegration(), CeleryIntegration()]
+    )
+
 from archives.lib.extensions import inject_extensions
 from archives.lib.requests import (before_request, cache_breaker,
                                    check_csrf_token, connect_redis,
                                    connect_sql, disconnect_redis,
                                    disconnect_sql, set_cookie)
 from archives.views import archive, graph
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, current_app
 
 app = Flask(__name__)
 
@@ -45,6 +58,9 @@ def notfound_error(e):
 if not app.config['DEBUG']:
     @app.errorhandler(Exception)
     def production_error(e):
+        if sentry_sdk:
+            sentry_sdk.capture_exception()
+
         if request.is_xhr:
             if 'debug' not in request.args and 'debug' not in request.form:
                 raise
